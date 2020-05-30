56e7a9f178e03604d1c245ab9ccde6fd10bcfdae
vapvin <tpehdj12@daum.net>
2020-05-30 22:37:00 +0900

    Fucking Hell

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use rocket::*;
+use rocket_contrib::{json::Json, serve::StaticFiles, templates::Template};
+use std::path::Path;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+mod lib;
+
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}! Welcome My Server with Rocket!", name)
 }
+
+#[get("/bye/<name>")]
+fn bye(name: String) -> String { format!("Bye {} ", name)}
 
 fn main() {
     rocket::ignite().mount("/", routes![hello]).launch();
